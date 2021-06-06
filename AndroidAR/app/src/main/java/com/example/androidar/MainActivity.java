3a39275c24df18ea479fbb0a04698ee4ddfeb774
Darshan Prajapati <dprajapati5206@gmail.com>
2021-06-05 21:52:40 -0400

    Code added

--- AndroidAR/app/src/main/java/com/example/androidar/MainActivity.java
+++ AndroidAR/app/src/main/java/com/example/androidar/MainActivity.java
@@ -6,40 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
-import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    // object of ArFragment Class
-    private ArFragment arCam;
+    private ArFragment arFragment;
 
-    // helps to render the 3d model
-    // only once when we tap the screen
     private int clickNo = 0;
 
-    public static boolean checkSystemSupport(Activity activity) {
+    public static boolean checkSystem(Activity activity) {
 
-        // checking whether the API version of the running Android >= 24
-        // that means Android Nougat 7.0
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             String openGlVersion = ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE))).getDeviceConfigurationInfo().getGlEsVersion();
 
-            // checking whether the OpenGL version >= 3.0
             if (Double.parseDouble(openGlVersion) >= 3.0) {
                 return true;
             } else {
@@ -59,21 +48,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (checkSystemSupport(this)) {
+        if (checkSystem(this)) {
 
-            // ArFragment is linked up with its respective id used in the activity_main.xml
-            arCam = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-            arCam.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+            Objects.requireNonNull(arFragment).setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
                 clickNo++;
-                // the 3d model comes to the scene only
-                // when clickNo is one that means once
                 if (clickNo == 1) {
                     Anchor anchor = hitResult.createAnchor();
                     ModelRenderable.builder()
                             .setSource(this, R.raw.gladiador)
                             .setIsFilamentGltf(true)
                             .build()
-                            .thenAccept(modelRenderable -> addModel(anchor, modelRenderable))
+                            .thenAccept(modelRenderable -> add(anchor, modelRenderable))
                             .exceptionally(throwable -> {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                                 builder.setMessage("Somthing is not right" + throwable.getMessage()).show();
@@ -84,20 +70,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void addModel(Anchor anchor, ModelRenderable modelRenderable) {
+    private void add(Anchor anchor, ModelRenderable modelRenderable) {
 
-        // Creating a AnchorNode with a specific anchor
         AnchorNode anchorNode = new AnchorNode(anchor);
-
-        // attaching the anchorNode with the ArFragment
-        anchorNode.setParent(arCam.getArSceneView().getScene());
-
-        // attaching the anchorNode with the TransformableNode
-        TransformableNode model = new TransformableNode(arCam.getTransformationSystem());
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
         model.setParent(anchorNode);
-
-        // attaching the 3d model with the TransformableNode
-        // that is already attached with the node
         model.setRenderable(modelRenderable);
         model.select();
     }
